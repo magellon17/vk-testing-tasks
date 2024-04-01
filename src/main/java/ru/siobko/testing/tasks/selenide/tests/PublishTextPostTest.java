d5993dbf903a66e57efcf188862e6fb07c6c4deb
boris <borissiobko@gmail.com>
2024-04-01 15:43:41 +0300

    Resolve "Open hover post more actions menu on profile page" Issue

--- src/main/java/ru/siobko/testing/tasks/selenide/tests/PublishTextPostTest.java
+++ src/main/java/ru/siobko/testing/tasks/selenide/tests/PublishTextPostTest.java
@@ -1,17 +1,31 @@
 package ru.siobko.testing.tasks.selenide.tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.siobko.testing.tasks.selenide.pages.main.FeedPage;
+import ru.siobko.testing.tasks.selenide.pages.main.LoginPage;
 import ru.siobko.testing.tasks.selenide.pages.main.MyProfilePage;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PublishTextPostTest extends BaseTest {
+    @BeforeAll
+    public static void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://ok.ru";
+        Selenide.open("/");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(email, password);
+    }
+
     @Test
     public void testPublishPost() {
         FeedPage feedPage = new FeedPage();
@@ -26,16 +40,11 @@ public class PublishTextPostTest extends BaseTest {
 
     @AfterAll
     public static void tearDown() {
-        MyProfilePage myProfilePage = new MyProfilePage();
-        SelenideElement moreActionsMenu = myProfilePage.getLastFeedPost()
-                .$(byXpath(".//div[@data-l='t,feed-actions-menu']"));
-
-        moreActionsMenu.scrollIntoView(true);
-        moreActionsMenu
-                .hover()
-                .$(byXpath(".//a[@data-l='t,.r']"))
-                .click();
-
+        $(byXpath(".//div[@data-l='t,feed-actions-menu']"))
+                .shouldBe(visible)
+                .scrollIntoView("{block: \"center\"}")
+                .hover();
+        $(byXpath(".//*[text()='Удалить заметку']")).shouldBe(visible).click();
         clearBrowserCache();
         Selenide.closeWebDriver();
     }
